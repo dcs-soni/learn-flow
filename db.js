0e8b52676c6f9946e421e4e7e8f8975cd8ae75d4
dcs-soni <52232900+dcs-soni@users.noreply.github.com>
2024-12-24 22:23:35 +0530

    Add sample db and importing db in admin

---
+++ db.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://admin:duggu123@100xusingmongo.1zwme.mongodb.net/learnflow"
+);
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = new Schema({
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
+});
+
+const adminSchema = new Schema({
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
+});
+
+const courseSchema = new Schema({
+  title: String,
+  description: String,
+  price: Number,
+  imageUrl: String,
+  creatorId: ObjectId,
+});
+
+const purchaseSchema = new Schema({
+  userId: ObjectId,
+  courseId: ObjectId,
+});
+
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("courses", courseSchema);
+const purchaseModel = mongoose.model("purchases", purchaseSchema);
+
+module.exports = {
+  userModel,
+  adminModel,
+  courseModel,
+  purchaseModel,
+};
